af5d855953633174ccd247f83e764372d4ff4c0f
James Homer <james@homerjam.es>
2020-01-23 12:26:38 +0000

    wheel

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import VueAvoid from "./lib/avoid";
-import VueCompass from "./lib/compass";
-import VueFlickity from "./lib/flickity";
-import VueIntersect from "./lib/intersect";
-import VueIsotope from "./lib/isotope";
-import VueMorph from "./lib/morph";
-import VueResponsiveBackgroundImage from "./lib/responsive-background-image";
-import VueSmartLink from "./lib/smart-link";
-import VueTouch from "./lib/touch";
+import VueAvoid from './lib/avoid';
+import VueCompass from './lib/compass';
+import VueFlickity from './lib/flickity';
+import VueIntersect from './lib/intersect';
+import VueIsotope from './lib/isotope';
+import VueMorph from './lib/morph';
+import VueResponsiveBackgroundImage from './lib/responsive-background-image';
+import VueSmartLink from './lib/smart-link';
+import VueTouch from './lib/touch';
+import VueWheel from './lib/wheel';
 
 export {
   VueAvoid,
@@ -17,5 +18,6 @@ export {
   VueMorph,
   VueResponsiveBackgroundImage,
   VueSmartLink,
-  VueTouch
+  VueTouch,
+  VueWheel,
 };
