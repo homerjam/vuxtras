39c30c8c7fcde7bacf014961b3c32281db9f24f8
James Homer <james@homerjam.es>
2021-06-28 16:20:22 +0100

    follow: update positioning

--- src/follow.vue
+++ src/follow.vue
@@ -39,6 +39,8 @@ export default {
   data() {
     return {
       isVisible: this.visible,
+      left: 0,
+      top: 0,
       x: 0,
       y: 0,
       raf: null,
@@ -47,22 +49,40 @@ export default {
   mounted() {
     document.addEventListener('mousemove', this.mousemove);
 
-    this.$el.parentNode.addEventListener('mouseenter', this.show);
-    this.$el.parentNode.addEventListener('mouseleave', this.hide);
+    if (this.$el.parentNode) {
+      // this.$el.parentNode.style.overflow = 'hidden';
+
+      this.$el.parentNode.addEventListener('mouseenter', this.show);
+      this.$el.parentNode.addEventListener('mouseleave', this.hide);
+    }
   },
   beforeDestroy() {
     document.removeEventListener('mousemove', this.mousemove);
 
-    this.$el.parentNode.removeEventListener('mouseenter', this.show);
-    this.$el.parentNode.removeEventListener('mouseleave', this.hide);
+    if (this.$el.parentNode) {
+      this.$el.parentNode.removeEventListener('mouseenter', this.show);
+      this.$el.parentNode.removeEventListener('mouseleave', this.hide);
+    }
   },
   methods: {
     mousemove(event) {
-      this.x = event.pageX;
-      this.y = event.pageY;
+      // this.x = event.pageX;
+      // this.y = event.pageY;
+
+      const rect = this.$el.parentElement.getBoundingClientRect();
+
+      this.left = rect.left;
+      this.top = rect.top;
+
+      this.x = event.x;
+      this.y = event.y;
     },
     animate() {
-      this.$el.style.transform = `translate(${this.x}px, ${this.y}px)`;
+      // this.$el.style.transform = `translate(${this.x}px, ${this.y}px)`;
+
+      this.$el.style.transform = `translate(${this.x - this.left}px, ${
+        this.y - this.top
+      }px)`;
 
       this.raf = window.requestAnimationFrame(this.animate);
     },
